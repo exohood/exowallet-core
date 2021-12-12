6b88b2a935138851478742adb043f4d24241a048
Exohood <82610137+exohood@users.noreply.github.com>
2021-12-12 05:57:42 -0500

    Delete trust-core directory

--- trust-core/GYB/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
-find . -name '*.gyb' |                                               \
-    while read file; do                                              \
-        "$DIR/gyb" --line-directive '' -o "${file%.gyb}" "$file"; \
-    done
